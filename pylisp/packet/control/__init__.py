830eca2db931186b1525a6484ac33b439d5b5e8f
Sander Steffann <sander@steffann.nl>
2013-01-09 21:32:53 +0100

    Add DDT related packets

--- pylisp/packet/control/__init__.py
+++ pylisp/packet/control/__init__.py
@@ -10,8 +10,9 @@ from constants import *
 
 # Records
 # =======
-from map_reply_record import LISPMapReplyRecord
 from locator_record import LISPLocatorRecord
+from map_referral_record import LISPMapReferralRecord
+from map_reply_record import LISPMapReplyRecord
 
 # Packets
 # =======
@@ -20,4 +21,5 @@ from map_request import LISPMapRequestMessage
 from map_reply import LISPMapReplyMessage
 from map_register import LISPMapRegisterMessage
 from map_notify import LISPMapNotifyMessage
+from map_referral import LISPMapReferralMessage
 from encapsulated_control_message import LISPEncapsulatedControlMessage
