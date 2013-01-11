f8cadc1de1580da1db6b572313f6f3f4cb4d66c7
Sander Steffann <sander@steffann.nl>
2013-01-11 20:49:26 +0100

    Fix inheritance and remove local __repr__ implementations where they are already inherited from the parent

--- pylisp/packet/ip/udp.py
+++ pylisp/packet/ip/udp.py
@@ -19,13 +19,6 @@ class UDPMessage(Protocol):
         self.checksum = checksum
         self.payload = payload
 
-    def __repr__(self):
-        # This works as long as we accept all properties as paramters in the
-        # constructor
-        params = ['%s=%r' % (k, v) for k, v in self.__dict__.iteritems()]
-        return '%s(%s)' % (self.__class__.__name__,
-                           ', '.join(params))
-
     def sanitize(self):
         '''
         Check if the current settings conform to the RFC and fix where possible
