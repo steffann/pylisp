3cd557d5b08d0d71ea1ccd26af0c72c3dceafd7f
Sander Steffann <sander@steffann.nl>
2013-06-01 00:16:48 +0200

    Fix class structure

--- pylisp/application/lispd/address_tree/__init__.py
+++ pylisp/application/lispd/address_tree/__init__.py
@@ -7,3 +7,6 @@ from .exceptions import *
 from .base import AbstractNode
 from .container_node import ContainerNode
 from .ddt_referral_node import DDTReferralNode
+from .ddt_container_node import DDTContainerNode
+from .drop_node import DropNode
+from .natively_forward_node import NativelyForwardNode
---
+++ pylisp/application/lispd/address_tree/ddt_container_node.py
@@ -0,0 +1,10 @@
+'''
+Created on 1 jun. 2013
+
+@author: sander
+'''
+from .container_node import ContainerNode
+
+
+class DDTContainerNode(ContainerNode):
+    pass
