d54844038dd29b1dbc080d7e266df74bb5ee94ea
Sander Steffann <sander@steffann.nl>
2013-12-08 17:57:45 +0100

    Interim-commit

--- etc/lispd/settings.py
+++ etc/lispd/settings.py
@@ -1,13 +1,15 @@
 from pylisp.application.lispd.address_tree.container_node import ContainerNode
 from pylisp.utils.auto_addresses import get_ipv4_address, get_ipv6_address
 from pylisp.application.lispd.address_tree.authoritative_container_node import AuthContainerNode
+from pylisp.application.lispd.address_tree.map_server_node import MapServerNode
 
 LISTEN_ON = [get_ipv4_address('eth0'),
              get_ipv6_address('eth0')]
 
 INSTANCES = {0: {1: ContainerNode(prefix=u'0.0.0.0/0',
                                   children=[AuthContainerNode(prefix='37.77.60.0/24',
-                                                              children=[],
+                                                              children=[MapServerNode(prefix='37.77.60.0/24',
+                                                                                      )],
                                                               ddt_nodes=LISTEN_ON)]),
                  2: ContainerNode(prefix=u'::/0',
                                   children=[AuthContainerNode(prefix='2A00:8640:B000::/36',
--- pylisp/application/lispd/address_tree/map_server_node.py
+++ pylisp/application/lispd/address_tree/map_server_node.py
@@ -4,6 +4,10 @@ Created on 1 jun. 2013
 @author: sander
 '''
 from pylisp.application.lispd.address_tree.base import AbstractNode
+from pylisp.application.lispd.received_message import ReceivedMessage
+from pylisp.application.lispd.utils.prefix import determine_instance_id_and_afi
+from pylisp.packet.lisp.control.map_register import MapRegisterMessage
+from pylisp.packet.lisp.control.map_register_record import MapRegisterRecord
 import logging
 
 
@@ -12,5 +16,46 @@ logger = logging.getLogger(__name__)
 
 
 class MapServerNode(AbstractNode):
+    def __init__(self, prefix, key, allow_more_specifics=True):
+        super(MapServerNode, self).__init__(prefix)
+        self.key = key
+        self.allow_more_specifics = allow_more_specifics
+
+    def handle_map_register_record(self, received_message, record, control_plane_sockets, data_plane_sockets):
+        assert isinstance(received_message, ReceivedMessage)
+        assert isinstance(record, MapRegisterRecord)
+
+        map_register = received_message.message
+        assert isinstance(map_register, MapRegisterMessage)
+
+        # Before we go any further we check the authentication data
+        if not map_register.verify_authentication_data(self.key):
+            logger.error(u"Ignoring a MapRegister message for {0} "
+                          "with invalid authentication data".format(record.eid_prefix))
+            return
+
+        # Extract the prefix info
+        dummy, dummy, prefix = determine_instance_id_and_afi(record.eid_prefix)
+
+        # Check for invalid prefixes
+        if not prefix.overlaps(self.prefix) \
+        or prefix.prefixlen < self.prefix.prefixlen:
+            logger.error(u"MapRegister message received by Map-Server {0} for prefix {1}".format(self.prefix, prefix))
+            return
+
+        # Check for more-specifics
+        if prefix.prefixlen > self.prefix.prefixlen \
+        and not self.allow_more_specifics:
+            logger.error(u'ETR tried to register more-specific {0} in {1}'.format(prefix, self.prefix))
+            return
+
+        # Store the data for now
+        if prefix.prefixlen not in self.registrations:
+            self.registrations[prefix.prefixlen] = {}
+
+        if received_message.source not in self.registrations[prefix.prefixlen]:
+            self.registrations[prefix.prefixlen][received_message.source]
+        self.registrations[prefix.prefixlen]
+
     def handle_map_request(self, received_message, control_plane_sockets, data_plane_sockets):
         pass
